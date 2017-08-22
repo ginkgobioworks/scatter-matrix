e74a26ac708c2fd28e6e0f3418c12722f6f9fdbb
Dan Cahoon <dpcahoon@gmail.com>
2017-08-22 14:22:34 -0400

    Mostly working build with rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,19 @@
+import postcss from 'rollup-plugin-postcss';
 import pkg from './package.json';
 
-export default [
-  {
-    entry: 'src/scatter-matrix.js',
-    dest: pkg.browser,
-    format: 'umd',
-    moduleName: 'scatter-matrix',
+export default {
+  input: 'src/scatter-matrix.js',
+  external: ['d3'],
+  globals: {
+    d3: 'd3',
   },
-  {
-    entry: 'src/scatter-matrix.js',
-    targets: [
-      { dest: pkg.main, format: 'cjs' },
-      { dest: pkg.module, format: 'es' },
-    ],
-  },
-];
+  name: 'ScatterMatrix',
+  output: [
+    { file: pkg.browser, format: 'umd' },
+    { file: pkg.main, format: 'cjs' },
+    { file: pkg.module, format: 'es' },
+  ],
+  plugins: [
+    postcss(),
+  ],
+};
